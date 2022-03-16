ff31d21b23c76b0818af64308123ad44379ad24a
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-16 03:23:53 -0400

    ex_2 - 3

--- ex_2/ex_2.r
+++ ex_2/ex_2.r
@@ -3,3 +3,21 @@ a <- 1
 b <- 2
 c <- 3
 (a%%1==0 & b%%1==0 & c%%1==0) & (a!=b | a!=c)
+
+# 3
+adequacao_peso = 90
+estado_nutricional = ''
+if (adequacao_peso <= 70) {
+    estado_nutricional = 'Desnutricao Grave'
+} else if (adequacao_peso >= 70 & adequacao_peso < 80) {
+    estado_nutricional = 'Desnutricao Moderada'
+} else if (adequacao_peso >= 80 & adequacao_peso < 90) {
+    estado_nutricional = 'Desnutricao Leve'
+} else if (adequacao_peso >= 90 & adequacao_peso < 110) {
+    estado_nutricional = 'Eutrofia (Peso Ideal)'
+} else if (adequacao_peso >= 110 & adequacao_peso < 120) {
+    estado_nutricional = 'Sobrepeso'
+} else {
+    estado_nutricional = 'Obesidade'
+}
+print(estado_nutricional)
