770c3a9873db335d0605e77ffa16fa993666ac2b
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-03-17 23:03:26 -0400

    ex_2, prv - final

--- ex_2/prv/ex_2.r
+++ ex_2/prv/ex_2.r
@@ -1,7 +1,7 @@
 # 1
 # a)
 vetor = c(3,3,4,6,4,2,2,3,6,5,4,2,3,5,5,3,1,2,1,5,2,5,5,1,5,6,3,4,6,3)
-matriz = matrix(data=vetor, nrow=6, ncol=5, byrow=T)
+matriz = matrix(data=vetor, nrow=10, ncol=5, byrow=T)
 matriz_uniq = unique(matriz)
 matrizUnicos = function(matriz) {
     vetor = numeric(0)
@@ -13,6 +13,7 @@ matrizUnicos = function(matriz) {
     return(vetor)
 }
 matriz_unicos = matrizUnicos(matriz)
+porcentagem_votos = numeric(0)
 for (i in matriz_unicos) {
     ocorrencias = 0
     for (j in matriz) {
@@ -20,8 +21,9 @@ for (i in matriz_unicos) {
             ocorrencias = ocorrencias + 1
         }
     }
+    porcentagem_votos = c(porcentagem_votos, ocorrencias/length(vetor)*100)
     if (i != 5 & i != 6) {
-        print(paste(c('Candidato: ',i,', votos: ', ocorrencias, ', porcentagam: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
+        print(paste(c('Candidato: ',i,', votos: ', ocorrencias, ', porcentagem: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
     }
 }
 
@@ -34,29 +36,88 @@ for (i in matriz_unicos) {
         }
     }
     if (i == 5) {
-        print(paste(c('Nulos (',i,') - votos: ', ocorrencias, ', porcentagam: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
+        print(paste(c('Nulos (',i,') - votos: ', ocorrencias, ', porcentagem: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
     } else if (i == 6) {
-        print(paste(c('Brancos (',i,') - votos: ', ocorrencias, ', porcentagam: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
+        print(paste(c('Brancos (',i,') - votos: ', ocorrencias, ', porcentagem: ', ocorrencias/length(vetor)*100, '%'), collapse=''))
     }
 }
 
 # c)
-matriz_unicos
-porcentagem_votos
 venceu = FALSE
 for (i in porcentagem_votos) {
-    if (i > 50) {
-        venceu = TRUE
-    }
+	if (i > 50) {
+        	venceu = TRUE
+        }
 }
 if (venceu == TRUE) {
-    vencedor = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
-    print(paste('O Candidato', vencedor, 'venceu no primeiro turno'))
+	vencedor = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
+        print(paste('O Candidato', vencedor, 'venceu no primeiro turno'))
 } else {
-    primeiro_candidato = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
-    matriz_unicos = matriz_unicos[-match(max(porcentagem_votos), porcentagem_votos)]
-    porcentagem_votos = porcentagem_votos[-match(max(porcentagem_votos), porcentagem_votos)]
-    segundo_candidato = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
-    print(paste('Os canditados ', primeiro_candidato, segundo_candidato, 'vao para o segundo turno'))
+	primeiro_candidato = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
+        matriz_unicos = matriz_unicos[-match(max(porcentagem_votos), porcentagem_votos)]
+        porcentagem_votos = porcentagem_votos[-match(max(porcentagem_votos), porcentagem_votos)]
+        segundo_candidato = matriz_unicos[match(max(porcentagem_votos), porcentagem_votos)]
+        print(paste('Os canditados ', primeiro_candidato, segundo_candidato, 'vao para o segundo turno'))
 }
 
+
+# 2
+numeros_aleatorios = sample(c(-10:-1,1:10))
+print(numeros_aleatorios)
+
+is.positivo = function (x) {
+	if (x > 0) {
+		return(TRUE)
+	}
+	return(FALSE)
+}
+
+# a)
+imprimePositivos = function (i) {
+	if (is.positivo(i)) {
+		print(i)
+	}
+}
+
+contaPositivos = function (i) {
+	if (is.positivo(i)) {
+		return(1)
+	}
+	return(0)
+}
+
+somaPositivos = function (i) {
+	if (is.positivo(i)) {
+		return(i)
+	}
+	return(0)
+}
+
+sapply(numeros_aleatorios, imprimePositivos)
+sum(sapply(numeros_aleatorios, contaPositivos))
+sum(sapply(numeros_aleatorios, somaPositivos))
+
+# b)
+imprimeNegativos = function (i) {
+	if (!is.positivo(i)) {
+		print(i)	
+	}
+}
+
+contaNegativos = function (i) {
+	if (!is.positivo(i)) {
+		return(1)
+	}
+	return(0)
+}
+
+somaNegativos = function (i) {
+	if (!is.positivo(i)) {
+		return(i)
+	}
+	return(0)
+}
+
+sapply(numeros_aleatorios, imprimeNegativos)
+sum(sapply(numeros_aleatorios, contaNegativos))
+sum(sapply(numeros_aleatorios, somaNegativos))
