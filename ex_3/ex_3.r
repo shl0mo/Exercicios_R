20b8b99a0418ff07a3a05dae6bb96d05ec9cf682
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-04-19 18:53:06 -0400

    ex_3 - 1.b - 1

---
+++ ex_3/ex_3.r
@@ -0,0 +1,15 @@
+# 1
+## a)
+especies = split(iris, 'Species')
+#comprimento = iris$Sepal.Length
+
+## b)
+#especies.Sepal.Length
+#tapply(especies,'Species', summary('Sepal.Length'))
+nomes_colunas = names(iris)
+vetor_especies = unique(iris$Species)
+for (i in vetor_especies) {
+	subset_especie = subset(iris, Species == i)
+	print(paste('Resumo - comprimento - ', i,':'))
+	print(summary(subset_especie$Sepal.Length))	
+}
